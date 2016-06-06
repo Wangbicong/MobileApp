dda4bff8fb9a1d9c99e663c478e3ed56ddb447ff
ylxdzsw <ylxdzsw@gmail.com>
2016-06-06 11:28:13 +0800

    done

--- app/src/main/assets/www/screen.js
+++ app/src/main/assets/www/screen.js
@@ -1,4 +1,21 @@
-function setContent(x) {
+// x::string - the expression
+// y::float - the result (OPTIONAL)
+function setContent(x, y) {
+    document.getElementById('screen').innerHTML = render(x, y)
+}
+
+function calculate(x) {
+    try {
+        return ''+eval(x)
+    } catch(e) {
+        return 'error'
+    }
+}
+
+function render(x, y) {
+    if (x == 'error')
+        return '<span class="er"> Error </span>'
+
     x = x.replace(/\*/g, '×')
          .replace(/\//g, '÷')
          .replace(/([\+\-×÷])/g, '<span class="op"> $1 </span>')
@@ -9,6 +26,9 @@ function setContent(x) {
         x = '<span> </span>'
     }
 
-    document.getElementById("screen").innerHTML = x
-}
+    if (y != null) {
+        x += '<span class="rs"> = ' + y + '</span>'
+    }
 
+    return x
+}
--- app/src/main/java/com/ylxdzsw/calculator/Calculator.java
+++ app/src/main/java/com/ylxdzsw/calculator/Calculator.java
@@ -6,15 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
 
-import com.ylxdzsw.calculator.R;
-
 enum Status {
     INPUT, RESULT, ERROR
 }
 
 public class Calculator extends AppCompatActivity {
     private StringBuilder expression;
-    private float result;
+    private String result;
     private WebView webView;
     private Status status;
 
@@ -25,53 +23,84 @@ public class Calculator extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
 
         expression = new StringBuilder();
-        webView = (WebView) findViewById(R.id.webView);
-        status = Status.INPUT;
+        webView    = (WebView) findViewById(R.id.webView);
+        status     = Status.INPUT;
+        result     = "";
 
-        findViewById(R.id.n0).setOnClickListener(v -> appendNumber(0));
-        findViewById(R.id.n1).setOnClickListener(v -> appendNumber(1));
-        findViewById(R.id.n2).setOnClickListener(v -> appendNumber(2));
-        findViewById(R.id.n3).setOnClickListener(v -> appendNumber(3));
-        findViewById(R.id.n4).setOnClickListener(v -> appendNumber(4));
-        findViewById(R.id.n5).setOnClickListener(v -> appendNumber(5));
-        findViewById(R.id.n6).setOnClickListener(v -> appendNumber(6));
-        findViewById(R.id.n7).setOnClickListener(v -> appendNumber(7));
-        findViewById(R.id.n8).setOnClickListener(v -> appendNumber(8));
-        findViewById(R.id.n9).setOnClickListener(v -> appendNumber(9));
+        findViewById(R.id.n0).setOnClickListener(v -> appendNumber('0'));
+        findViewById(R.id.n1).setOnClickListener(v -> appendNumber('1'));
+        findViewById(R.id.n2).setOnClickListener(v -> appendNumber('2'));
+        findViewById(R.id.n3).setOnClickListener(v -> appendNumber('3'));
+        findViewById(R.id.n4).setOnClickListener(v -> appendNumber('4'));
+        findViewById(R.id.n5).setOnClickListener(v -> appendNumber('5'));
+        findViewById(R.id.n6).setOnClickListener(v -> appendNumber('6'));
+        findViewById(R.id.n7).setOnClickListener(v -> appendNumber('7'));
+        findViewById(R.id.n8).setOnClickListener(v -> appendNumber('8'));
+        findViewById(R.id.n9).setOnClickListener(v -> appendNumber('9'));
 
-        findViewById(R.id.lp)   .setOnClickListener(v -> appendOperator('('));
-        findViewById(R.id.rp)   .setOnClickListener(v -> appendOperator(')'));
+        findViewById(R.id.lp)   .setOnClickListener(v -> appendNumber('('));
+        findViewById(R.id.rp)   .setOnClickListener(v -> appendNumber(')'));
+        findViewById(R.id.dot)  .setOnClickListener(v -> appendNumber('.'));
+
         findViewById(R.id.plus) .setOnClickListener(v -> appendOperator('+'));
         findViewById(R.id.minus).setOnClickListener(v -> appendOperator('-'));
         findViewById(R.id.mul)  .setOnClickListener(v -> appendOperator('*'));
         findViewById(R.id.div)  .setOnClickListener(v -> appendOperator('/'));
-        findViewById(R.id.dot)  .setOnClickListener(v -> appendOperator('.'));
 
         findViewById(R.id.ac)  .setOnClickListener(v -> clearAll());
         findViewById(R.id.back).setOnClickListener(v -> deleteLastOne());
         findViewById(R.id.eq)  .setOnClickListener(v -> calculate());
+        findViewById(R.id.eq)  .setOnLongClickListener(v -> ans());
 
         webView.loadUrl("file:///android_asset/www/screen.html");
         webView.getSettings().setJavaScriptEnabled(true);
     }
 
-    private void appendNumber(int x) {
-        expression.append(x);
+    private void appendNumber(char x) {
+        switch (status) {
+            case ERROR:
+            case RESULT:
+                clearAll();
+            case INPUT:
+                expression.append(x);
+                break;
+        }
         updateView();
     }
 
     private void appendOperator(char x) {
-        expression.append(x);
+        switch (status) {
+            case ERROR:
+                clearAll();
+                expression.append(x);
+                break;
+            case RESULT:
+                clearAll();
+                expression.append(result).append(x);
+                break;
+            case INPUT:
+                expression.append(x);
+                break;
+        }
         updateView();
     }
 
-    private void deleteLastOne() {
-        if (status == Status.INPUT) {
-            expression.deleteCharAt(expression.length() - 1);
-            updateView();
-        } else {
+    private boolean ans() {
+        if (status != Status.INPUT) {
             clearAll();
         }
+        expression.append(result);
+        updateView();
+        return true;
+    }
+
+    private void deleteLastOne() {
+        int l = expression.length();
+        if (l > 0) {
+            expression.deleteCharAt(l - 1);
+        }
+        status = Status.INPUT;
+        updateView();
     }
 
     private void clearAll() {
@@ -81,11 +110,36 @@ public class Calculator extends AppCompatActivity {
     }
 
     private void calculate() {
-        webView.evaluateJavascript("calculate('"+this.expression+"');", val -> Log.wtf("calculator", val));
+        if (status != Status.INPUT) {
+            return;
+        }
+
+        webView.evaluateJavascript("calculate('"+this.expression+"');", val -> {
+            val = val.substring(1, val.length()-1);
+
+            if (val.equals("error")) {
+                status = Status.ERROR;
+            } else {
+                status = Status.RESULT;
+                result = val;
+            }
+
+            updateView();
+        });
     }
 
     private void updateView() {
-        webView.evaluateJavascript("setContent('"+this.expression+"');", val -> {});
+        switch (status) {
+            case ERROR:
+                webView.evaluateJavascript("setContent('error');", val -> {});
+                break;
+            case RESULT:
+                webView.evaluateJavascript("setContent('"+expression+"',"+result+");", val -> {});
+                break;
+            case INPUT:
+                webView.evaluateJavascript("setContent('"+expression+"');", val -> {});
+                break;
+        }
     }
 
 }
