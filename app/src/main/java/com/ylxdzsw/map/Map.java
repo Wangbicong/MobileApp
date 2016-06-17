4b5314c5989ad047686d069903dabcb8e807ca4b
ylxdzsw <ylxdzsw@gmail.com>
2016-06-18 02:29:05 +0800

    map

--- app/src/main/java/com/ylxdzsw/map/Map.java
+++ app/src/main/java/com/ylxdzsw/map/Map.java
@@ -1,10 +1,23 @@
 package com.ylxdzsw.map;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.CameraPosition;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.services.geocoder.GeocodeAddress;
+import com.amap.api.services.geocoder.GeocodeQuery;
+import com.amap.api.services.geocoder.GeocodeResult;
+import com.amap.api.services.geocoder.GeocodeSearch;
+import com.amap.api.services.geocoder.RegeocodeResult;
 import com.ylxdzsw.kit.R;
 
 public class Map extends AppCompatActivity {
@@ -14,10 +27,14 @@ public class Map extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_map);
 
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
+
+        findViewById(R.id.pos).setOnClickListener(v -> getLatlntPos());
+        findViewById(R.id.describe).setOnClickListener(v -> getDescribePos());
+        findViewById(R.id.current).setOnClickListener(v -> getCurrentPos());
 
         aMap = mapView.getMap();
     }
@@ -44,5 +61,77 @@ public class Map extends AppCompatActivity {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    private void moveTo(double x, double y) {
+        CameraPosition pos = new CameraPosition(new LatLng(x, y), 16, 0, 0);
+        aMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
+    }
+
+    private void getLatlntPos() {
+        EditText editText = new EditText(this);
+
+        new AlertDialog.Builder(this)
+            .setTitle("输入经纬度，英文逗号分隔")
+            .setIcon(android.R.drawable.ic_dialog_info)
+            .setView(editText)
+            .setNegativeButton("取消", null)
+            .setPositiveButton("确定", (d, which) -> {
+                String[] x = editText.getText().toString().split(",");
+                try {
+                    moveTo(Double.valueOf(x[0]), Double.valueOf(x[1]));
+                } catch (Exception e) {
+                    Toast.makeText(this, "经纬度格式错误", Toast.LENGTH_SHORT).show();
+                }})
+            .show();
+    }
+
+    private void getDescribePos() {
+        EditText editText = new EditText(this);
+
+        new AlertDialog.Builder(this)
+                .setTitle("输入要查询的位置(哈尔滨内)")
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setView(editText)
+                .setNegativeButton("取消", null)
+                .setPositiveButton("确定", (d, which) -> {
+                    GeocodeSearch geocodeSearch = new GeocodeSearch(this);
+                    geocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+                        @Override
+                        public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+
+                        }
+
+                        @Override
+                        public void onGeocodeSearched(GeocodeResult result, int i) {
+                            GeocodeAddress address = result.getGeocodeAddressList().get(0);
+                            moveTo(address.getLatLonPoint().getLatitude(),
+                                   address.getLatLonPoint().getLongitude());
+                        }
+                    });
+                    GeocodeQuery query = new GeocodeQuery(editText.getText().toString(), "0451");
+                    geocodeSearch.getFromLocationNameAsyn(query);
+                })
+                .show();
+    }
+
+    private void getCurrentPos() {
+        AMapLocationClient client = new AMapLocationClient(getApplicationContext());
+        AMapLocationClientOption option = new AMapLocationClientOption();
+
+        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        option.setOnceLocation(true);
+
+        client.setLocationOption(option);
+        client.setLocationListener(location -> {
+            if (location == null) return;
+            if (location.getErrorCode() == 0) {
+                moveTo(location.getLatitude(), location.getLongitude());
+            } else {
+                Toast.makeText(this, "定位失败：" + location.getErrorInfo(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        client.startLocation();
     }
 }
